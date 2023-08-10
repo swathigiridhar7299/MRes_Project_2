43664a3f5d5c8936c91448049eaa31fde46a83e9
swathigiridhar7299 <116357325+swathigiridhar7299@users.noreply.github.com>
2023-08-10 19:49:34 +0100

    Add files via upload

---
+++ PCA_Analysis.R
@@ -0,0 +1,22 @@
+#load libraries
+library("factoextra")
+library("FactoMineR")
+library(ggsci)
+library(ggpubr)
+library(RColorBrewer)
+library(umap)
+data <- read.csv('file.csv') 
+data <- na.omit(data)
+pca.data <- PCA(plot_data, scale.unit = TRUE, graph = FALSE)
+fviz_eig(pca.data, addlabels = TRUE, ylim = c(0, 70))
+b<-fviz_pca_var(pca.data, col.var = "cos2",
+                gradient.cols = c("#FFCC00", "#CC9933", "#660033", "#330033"),
+                repel = TRUE) 
+a <- fviz_pca_ind(pca.data, col.ind = data$Treatment,
+                  palette = "jco", addEllipses = FALSE, repel=FALSE, geom = "point")
+PCA_Niraparib <- ggpar(a,
+                       title = "Principal Component Analysis",
+                       xlab = "PC1", ylab = "PC2",
+                       legend.title = "Treatment", legend.position = "top",
+                       ggtheme = theme_minimal())
+PCA_Niraparib
